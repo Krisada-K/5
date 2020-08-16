fd5449adc54079c074b3d6e524e94c1f17dbe677
Krisada Kareeso <kid-gamer@outlook.com>
2020-08-16 13:59:16 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  float b, h ,area ;
+  printf("Enter B : ");
+  scanf("%f", &b);
+  printf("Enter H : ");
+  scanf("%f", &h);
+  printf("Triangle area = %f", 0.5*b*h);
+  return 0;
+}
